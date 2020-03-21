695b5e49f5dfc6c487646ead1ca4a1aa33bcdaf0
hosi5678 <hosi5678@gmail.com>
2020-03-22 08:49:25 +0900

    文字化け対応修正

--- create_user.sql
+++ create_user.sql
@@ -2,3 +2,9 @@ drop user if exists 'user01'@'192.168.1.3';
 
 create user 'user01'@'192.168.1.3' identified by 'hosi3162';
 grant all privileges on myapp.* to user01@'192.168.1.3' identified by 'hosi3162';
+
+drop user if exists 'user02'@'192.168.1.7';
+
+create user 'user02'@'192.168.1.7' identified by 'hosi3162';
+grant all privileges on myapp.* to user02@'192.168.1.7' identified by 'hosi3162';
+
