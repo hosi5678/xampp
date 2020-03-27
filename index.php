784ae701de8635ded8aae16c335fd07705cecef2
hosi5678 <hosi5678@gmail.com>
2020-03-27 20:32:51 +0900

    文字化け対応、参考ファイル等追加

--- index.php
+++ index.php
@@ -1,28 +1,15 @@
 <?php
 
-// phpinfo();
-
-try {
-  $pdo = new PDO('mysql:dbname=myapp;host=192.168.1.3','myapp_user','hosi3162');
-	echo '<h1>myapp_userでpdo接続に成功。(dotinstall)</h1>';
-} catch (PDOException $e) {
-  exit('データベースに接続できませんでした。' . $e->getMessage());
+function getStats(...$numbers){
+	// return $a+$b+$c;
+	$total=0;
+	foreach($numbers as $number){
+		$total+=$number;
+	}
+	return [$total,$total/count($numbers)];
 }
 
-?>
+print_r(getStats(1,3,5));
+list($sum,$average)=getStats(1,3,5);
 
-<!doctype html>
-<html lang="ja">
-
-<head>
-</head>
-
-<body>
-
-<h1>
-	for access test.
-</h1>
-
-</body>
-
-</html>
+echo $sum.':'.$average;
