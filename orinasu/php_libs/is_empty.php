dfd70479f8fb530bda2a30839e10b0d43ce8cd14
hosi5678 <hosi5678@gmail.com>
2020-05-09 08:19:52 +0900

    ・先頭パスの修正

--- orinasu/php_libs/is_empty.php
+++ orinasu/php_libs/is_empty.php
@@ -3,7 +3,7 @@
 function is_empty($s){
 	if(empty($s)|!isset($s)){
 		echo '値がセットされていません。<br>';
-		echo '<a href=../index.php>先頭ページへ移動</a>';
+		echo '<a href=./index.php>先頭ページへ移動</a>';
 		exit();
 	}
 }
