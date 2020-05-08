e236d5fa92c193e15e43d97bd0b4a386e4850c6f
hosi5678 <hosi5678@gmail.com>
2020-05-09 08:17:07 +0900

    ・tag nameをparentで設定可能にした。

--- orinasu/js/delete_table.js
+++ orinasu/js/delete_table.js
@@ -2,15 +2,18 @@ function delete_table(event){
 
   var id=event.target.id;
   var table_name=event.target.table_name;
+  var parent_tag_str=event.target.parent_tag_str;
+  var prev=event.target.prev;
 
   var query='delete from '+table_name+' where id='+id+';';
 
-  // console.log(query);
+  console.log('in delete table:');
+  console.log('prev:'+prev);
 
   $.when(
       ajax_stmt_exec(table_name,query,'assoc'),
     ).done(function(){
-      select_from_table("select",table_name);
+      select_from_table(parent_tag_str,table_name);
   });
 
 }
