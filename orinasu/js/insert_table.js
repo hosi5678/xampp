77e3d4459ac05dbb2a186a7a2dd056cd3f19578b
hosi5678 <hosi5678@gmail.com>
2020-05-09 08:18:18 +0900

    ・tag nameをparentで設定可能にした。 ・非同期を実行後、tableを作成

--- orinasu/js/insert_table.js
+++ orinasu/js/insert_table.js
@@ -1,26 +1,44 @@
 function insert_table(event){
 
   var col=event.target.col;
-
-  col.shift(); // idは削除
-  console.log(col);
-  console.log(col.length);
-
-
+  var prev=event.target.prev;
   var table_name=event.target.table_name;
+  var parent_tag_str=event.target.parent_tag_str;
+
+  // col.shift(); // idは削除
+
+  console.log('-----in insert table-----');
+  console.log('table name:'+table_name);
+  console.log('prev:'+prev);
+  console.log('parent_tag_str:'+parent_tag_str);
+  console.log('col:'+col);
 
   var params=new Array();
 
+  var query_columns=new Array();
+
   for(var i=0;i<col.length;i++){
+    if(col[i]=='id') continue;
+      query_columns.push(col[i]);
+  }
+
+  console.log('query_columns:'+query_columns);
+
+  for(var i=0;i<query_columns.length;i++){
 
     var str=document.getElementById(table_name+(i+1)).value;
 
-    if(col[i]=='myouji'||col[i]=='namae'){
+    if(query_columns[i]=='myouji'||query_columns[i]=='namae'){
 
       // 悪意がある場合
       str=reject_str(str);
 
       if(str===1){
+        return false;
+      }
+
+      // 名前が空欄のとき
+      if(str==''){
         return false;
       }
 
@@ -30,27 +48,35 @@ function insert_table(event){
       params.push(str);
     }
 
-    if(col[i]=='sun') params[i]=0;
+    if(query_columns[i]=='sun') params[i]=0;
 
   }
 
   var query='insert into '+table_name+'(';
   
-  for(var i=0;i<col.length;i++){
-      query+=col[i];
-        if(i!=col.length-1) query+=',';
+  for(var i=0;i<query_columns.length;i++){
+      // if(col[i]=='id') continue;
+
+      query+=query_columns[i];
+        if(i!=query_columns.length-1) query+=',';
   }
 
   query+=') values(';
 
-  for(var i=0;i<col.length;i++){
+  for(var i=0;i<query_columns.length;i++){
     query+=params[i];
-       if(i!=col.length-1) query+=',';
+       if(i!=query_columns.length-1) query+=',';
   
   }
 
   query+=');';
 
-  ajax_stmt_exec(table_name,query,'assoc');
+  console.log('query:'+query);
+
+  $.when(
+    ajax_stmt_exec(table_name,query,'assoc'),
+  ).done(function(){
+    select_from_table(parent_tag_str,table_name);
+});
 
 }
