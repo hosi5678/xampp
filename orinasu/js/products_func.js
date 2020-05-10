fdddba957ddfb9a91036c716f4c43fea5a8f8d1d
hosi5678 <hosi5678@gmail.com>
2020-05-11 06:33:33 +0900

    高さの調整

--- orinasu/js/products_func.js
+++ orinasu/js/products_func.js
@@ -9,16 +9,19 @@ function products_func(){
     
     if(tmp=='visible'){
   
-      create_products_input_form(parent_tag_str,parent_tag_str);
+      create_products_input_form(parent_tag_str,table_name);
  //     select_from_table(parent_tag_str,table_name);
 
       document.getElementById(parent_tag_str+"_mark").innerText='販売記録の登録・削除▲';
-  
+      document.getElementById(parent_tag_str).style.height='auto';
+
     }else{
       document.getElementById(parent_tag_str+"_mark").innerText='販売記録の登録・削除▼';
-  
+      document.getElementById(parent_tag_str).style.height=0;
     }
   
     document.getElementById(parent_tag_str).style.visibility = tmp;
+    // $('#'+parent_tag_str).slideToggle();
+
   
   }
