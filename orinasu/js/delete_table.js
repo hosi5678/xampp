1dcefc9e103adb39d14726f901f9d6c17c83120c
hosi5678 <hosi5678@gmail.com>
2020-05-11 06:33:10 +0900

    パラメータを特定するのに数字は使用しない

--- orinasu/js/delete_table.js
+++ orinasu/js/delete_table.js
@@ -1,13 +1,28 @@
 function delete_table(event){
 
-  var id=event.target.id;
+  console.log('----- in delete table -----');
+
   var table_name=event.target.table_name;
   var parent_tag_str=event.target.parent_tag_str;
   var prev=event.target.prev;
+  var col=event.target.col;
+
+  var tds=$(this).closest('tr').children();
+
+  console.log(col);
+
+  var id;
+
+  for(var i=0;i<col.length;i++){
+    if(col[i]=='id'){
+      id=tds[i].innerText;
+    }     
+  }
+
 
   var query='delete from '+table_name+' where id='+id+';';
 
-  console.log('----- in delete table -----');
+  console.log('id:'+id);
   console.log('prev:'+prev);
   console.log('qeery:'+query);
 
