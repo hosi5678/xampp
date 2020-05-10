cdf6af3ce9f04e6447b1d1b56f5f9978fed61715
hosi5678 <hosi5678@gmail.com>
2020-05-11 06:28:43 +0900

    アクセスログの設置

--- orinasu/sql/init.sql
+++ orinasu/sql/init.sql
@@ -148,3 +148,16 @@ create view members_join as
 show create view members_join \G
 
 select * from members_join order by id asc;
+
+drop table if exists table_access_log;
+
+create table table_access_log(
+  id int unsigned not NULL AUTO_INCREMENT PRIMARY KEY ,
+  ip_address varchar(20) not NULL,
+  table_name varchar(50) not NULL ,
+  query text not null,
+  time datetime 
+);
+
+desc table_access_log \G
+
