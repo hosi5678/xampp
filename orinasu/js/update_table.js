cf6c706b15afa79d82b82340dbd6347df2f76104
hosi5678 <hosi5678@gmail.com>
2020-05-10 09:26:58 +0900

    ・備考の追加

--- orinasu/js/update_table.js
+++ orinasu/js/update_table.js
@@ -5,52 +5,84 @@ function update_table(event){
   var id=event.target.id;
   var parent_tag_str=event.target.parent_tag_str;
   var table_name=event.target.table_name;
+  var col=event.target.col;
+  var label=event.target.label;
 
   console.log('id:'+id);
   console.log('parent_tag_str'+parent_tag_str);
   console.log('table_name'+table_name);
+  console.log('col is:');
+  console.log(col);
+  console.log('label is:');
+  console.log(label);
 
-  $.when(
+  var update_val=new Array();
 
-    ajax_stmt_exec(table_name,"select * from "+table_name+" limit 0;",'column'),
+  for(var i=0;i<label.length;i++){
+    if(label[i]=='id') continue;
+    
+    if(label[i]=='姓'||label[i]=='名'){
+      var str=document.getElementById(parent_tag_str+i).value;
+ 
+      str=reject_str(str);
 
-    ).done(function(col_name){
-        // alert(table_name);
-        // alert(col_name);
-        // alert(id+col_name.length);
+      if(str==1){
+        return false;
+      }
+
+      update_val.push('"'+str+'"');
+
+    }else if(label[i]=='備考'){
+
+      var str=document.forms["form1"].elements["bikou"].value;
+
+      str=reject_str(str);
+
+      if(str==1){
+        return false;
+      }
+
+      update_val.push('"'+str+'"');
+
+    }else{
+      update_val.push(document.getElementById(table_name+i).value);
+    }
+
+  }
+
+  console.log('update val is:');
+  console.log(update_val);
+
+  // $.when(
+
+  //   ajax_stmt_exec(table_name,"select * from "+table_name+" limit 0;",'column'),
+
+  //   ).done(function(col_name){
      
-      var update_val=new Array();
+  //     var update_val=new Array();
 
-      for(var i=0;i<col_name.length;i++){
-          if(i==0) continue;
+  //     for(var i=0;i<col_name.length;i++){
+  //         if(i==0) continue;
           
-          if(i==1||i==2){
-            update_val.push('"'+document.getElementById(table_name+i).value+'"');
+  //         if(i==1||i==2){
+  //           update_val.push('"'+document.getElementById(table_name+i).value+'"');
 
-          }else{
-            update_val.push(document.getElementById(table_name+i).value);
-          }
+  //         }else{
+  //           update_val.push(document.getElementById(table_name+i).value);
+  //         }
       
-        }
+  //       }
 
-        // alert(update_val);
-
-        col_name.shift(); // idを除去
-
-        // alert(col_name);
-      // console.log(update_val);
-
-      // "update members set myouji=?,namae=?,sun=?,mon=?,tue=?,wed=?,thu=?,fri=?,sat=? where id=?;"
+      col.shift(); // idを除去
 
       var query='update '+table_name+' set ';
 
-      // console.log(query);
+      for(var i=0;i<col.length;i++){
+        // if(col[i]=='id') continue;
 
-      for(var i=0;i<col_name.length;i++){
+        query+=col[i]+'='+update_val[i];
 
-        query+=col_name[i]+'='+update_val[i];
-
-        if(i!=col_name.length-1){
+        if(i!=col.length-1){
           query+=',';
         }else{
           query+=' where id='+id+';';
@@ -59,7 +91,6 @@ function update_table(event){
       }
 
       console.log('query:'+query);
-      // alert(parent_tag_str);
 
       $.when(
         ajax_stmt_exec(table_name,query,'assoc'),
@@ -67,6 +98,6 @@ function update_table(event){
         select_from_table(parent_tag_str,table_name);
       });
 
-  });
+  // });
 
 }
