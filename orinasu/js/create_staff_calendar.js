3a70f693e42a04e6ce9c8c238f310ffe9f88c02c
hosi5678 <hosi5678@gmail.com>
2020-05-20 08:34:22 +0900

    カレンダーjs化の調査

--- orinasu/js/create_staff_calendar.js
+++ orinasu/js/create_staff_calendar.js
@@ -16,31 +16,45 @@ function create_staff_calendar(parent_tag_str,table_name,youbi){
     console.log(youbi);
 
     var today=new Date();
+    var thisYear=today.getFullYear();
+    var thisMonth=today.getMonth();
+    var thisDate=today.getDate();
 
-    console.log('今年:'+today.getFullYear());
-    console.log('今月(js+1):');
-    console.log(today.getMonth()+1);
-    console.log('今日の日にち:'+today.getDate());
+    var lastMonth_lastday=new Date(thisYear,thisMonth,0);
+
+
+    console.log('今月のカレンダーの開始日:'+(lastMonth_lastday.getDate()-lastMonth_lastday.getDay()));
+    console.log('今年は何年か?'+thisYear);
+    console.log('今月は何月か？(js+1):'+(thisMonth+1));
+    console.log('今日の何日か？:'+thisDate);
     // 曜日の取得
     console.log('今日の曜日:'+youbi[today.getDay()]);
 
+    // 先月の末尾を取得する
+    console.log('先月は何年か？'+lastMonth_lastday.getFullYear());
+    console.log('先月は何月か？'+(lastMonth_lastday.getMonth()+1));
+
+    console.log('先月の末尾:'+lastMonth_lastday.getDate());
+    console.log('先月の末尾の曜日番号:'+lastMonth_lastday.getDay());
+
+
     // 今月の末尾を取得する
-    today=new Date();
+    var thisMonth_lastday=new Date(thisYear,(thisMonth+1),0);
 
-    var thismonth_lastday=new Date(today.getFullYear(),today.getMonth()+1,0);
+    console.log('今月の末尾:'+thisMonth_lastday.getDate());
+    console.log('今月の末尾は何年か？:'+thisMonth_lastday.getFullYear()); // ->今月の末尾は同年
+    console.log('今月の末尾は何月か？:'+(thisMonth_lastday.getMonth()+1)); // ->今月の末尾は同月
 
-    console.log('今月の末尾:'+thismonth_lastday.getDate());
-    console.log('今月の末尾の曜日番号:'+thismonth_lastday.getDay());
-  
+
+    console.log('今月の末尾の曜日番号:'+thisMonth_lastday.getDay());
 
     // 今日の週番号を取得する
-    console.log('今日の週番号:'+getWeekNum(new Date()));
+    console.log('今日の週番号:'+getWeekNum(today));
 
     // 今月が何週あるか調べる
-    var today=new Date();
-    var thismonth=new Date(today.getFullYear(),today.getMonth()+1,0);
+    // var thismonth=new Date(thisYear,(thisMonth+1),0);
 
-    console.log('今月が何週あるか:'+getWeekNum(thismonth));
+    console.log('今月が何週あるか:'+getWeekNum(thisMonth_lastday));
 
     var today = new Date();
     var holidays = JapaneseHolidays.getHolidaysOf(today.getFullYear());
@@ -75,55 +89,63 @@ function create_staff_calendar(parent_tag_str,table_name,youbi){
 
         console.log('曜日:'+youbi[day.getDay()]);
 
+        console.log('test');
 
-    var form=document.createElement('form');
-    form.name=parent_tag_str;
+        var testDay=new Date(2019,13,1);
 
-    var p=document.createElement('p');
+        console.log(testDay.getFullYear());
+        console.log(testDay.getMonth()+1);
+        console.log(testDay.getDate());
 
-    p.innerText=today.getFullYear()+'年';
 
-    form.appendChild(p);
+    // var form=document.createElement('form');
+    // form.name=parent_tag_str;
 
-    var p=document.createElement('p');
+    // var p=document.createElement('p');
 
-    p.innerText=(today.getMonth()+1)+'月';
+    // p.innerText=today.getFullYear()+'年';
 
-    form.appendChild(p);
+    // form.appendChild(p);
 
-    var table=document.createElement('table');
+    // var p=document.createElement('p');
 
-    var tr=document.createElement('tr');
+    // p.innerText=(today.getMonth()+1)+'月';
 
-    for(var i=0;i<day.getDay()+1;i++){
-        var td=document.createElement('td');
-        td.innerText=today.getMonth()+'/'+(this_calendar_first_day+i);
-        tr.appendChild(td);
-    }
+    // form.appendChild(p);
 
-    for(var i=1;i<thismonth_lastday.getDate();i++){
-        var td=document.createElement('td');
-        td.innerText=today.getMonth()+1+'/'+i;
-        tr.appendChild(td);
+    // var table=document.createElement('table');
 
-        var inc_day=new Date(today.getFullYear(),today.getMonth(),i);
+    // var tr=document.createElement('tr');
 
-        if(inc_day.getDay()==6){
-            console.log('--true--');
-            console.log(inc_day.getFullYear());
-            console.log(inc_day.getDate());
-            // var tr=document.createElement('tr');
-        }
-        // if()
+    // for(var i=0;i<day.getDay()+1;i++){
+    //     var td=document.createElement('td');
+    //     td.innerText=today.getMonth()+'/'+(this_calendar_first_day+i);
+    //     tr.appendChild(td);
+    // }
 
-    }
+    // for(var i=1;i<thisMonth_lastday.getDate();i++){
+    //     var td=document.createElement('td');
+    //     td.innerText=today.getMonth()+1+'/'+i;
+    //     tr.appendChild(td);
 
-    tr.appendChild(td);
+    //     var inc_day=new Date(today.getFullYear(),today.getMonth(),i);
 
-    table.appendChild(tr);
+    //     if(inc_day.getDay()==6){
+    //         console.log('--true--');
+    //         console.log(inc_day.getFullYear());
+    //         console.log(inc_day.getDate());
+    //         // var tr=document.createElement('tr');
+    //     }
+    //     // if()
 
-    form.appendChild(table);
+    // }
 
-    parent_tag.appendChild(form);
+    // tr.appendChild(td);
+
+    // table.appendChild(tr);
+
+    // form.appendChild(table);
+
+    // parent_tag.appendChild(form);
 
 }
