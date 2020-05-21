388f1a43f1b71dd4adcc8af212254d2ed6019482
hosi5678 <hosi5678@gmail.com>
2020-05-21 15:01:53 +0900

    カレンダーの進行中

--- orinasu/js/create_calendar.js
+++ orinasu/js/create_calendar.js
@@ -9,26 +9,89 @@ function create_calendar(parent_tag_str,year,month,day){
          parent_tag.removeChild(parent_tag.firstChild);
      }
  
-     var form=document.createElement('form');
-     form.name=parent_tag_str;
+    var form=document.createElement('form');
+		 
+		form.name=parent_tag_str;
 
-     var div_title=document.createElement('div');
+    var div_title=document.createElement('div');
      
-     var div=document.createElement('div');
+    var div=document.createElement('div');
         div.id='year';
-        div.innerText=year;
-        div.style.display='inline-block';
-
-     div_title.appendChild(div);
-
-     var div=document.createElement('div');
-        div.id='month';
-        div.innerText=month;
+        div.value=year;
+        div.innerText=year+'年';
         div.style.display='inline-block';
 
     div_title.appendChild(div);
 
-     form.appendChild(div_title);
+    var div=document.createElement('div');
+      div.id='month';
+      div.value=month;
+      div.innerText=month+'月';
+      div.style.display='inline-block';
+
+    div_title.appendChild(div);
+
+		form.appendChild(div_title);
+		 
+		 var div=document.createElement('div');
+		 div.style.display='inline-block';
+		 div.innerText='<<';
+
+		 div.addEventListener('click',function(event){
+			//  console.log(div.innerText);
+			//  console.log(year);
+
+			 month=month-1;
+
+			 if(month==0){
+				 month=12;
+				 year=year-1;
+			 }
+
+			 create_calendar(parent_tag_str,year,month,day);
+		 });
+
+		div_title.appendChild(div);
+
+		var div=document.createElement('div');
+		div.style.display='inline-block';
+		div.innerText='今月';
+
+		div.addEventListener('click',function(event){
+			// console.log(div.innerText);
+			// console.log(year);
+
+			var this_month=new Date();
+
+			year=this_month.getFullYear();
+			month=this_month.getMonth()+1;
+
+			create_calendar(parent_tag_str,year,month,day);
+		});
+
+		div_title.appendChild(div);
+
+
+		var div=document.createElement('div');
+		div.style.display='inline-block';
+		div.innerText='>>';
+
+		div.addEventListener('click',function(event){
+			console.log(div.innerText);
+			console.log(year);
+
+			month=month+1;
+
+			if(month==13){
+				month=month-12;
+				year=year+1;
+			}
+
+			create_calendar(parent_tag_str,year,month,day);
+		});
+
+		div_title.appendChild(div);
+
 
     //  var p=document.createElement('p');
 
